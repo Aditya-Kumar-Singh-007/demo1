0642dc407c45ac263e48e93ae9f1d5021352b0f7
Aditya-Kumar-Singh-007 <132223386+Aditya-Kumar-Singh-007@users.noreply.github.com>
2024-03-12 13:45:06 +0530

    Add files via upload

---
+++ sum1.py
@@ -0,0 +1,11 @@
+# Simple Python Program
+
+# Get user input for two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform a basic calculation (addition in this case)
+result = num1 + num2
+
+# Display the result
+print("The sum of", num1, "and", num2, "is:", result)
